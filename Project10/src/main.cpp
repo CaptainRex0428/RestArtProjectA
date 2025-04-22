2b7c64eabe1441e02add698030f10c064462577c
Rex <rvo0428@163.com>
2025-04-22 21:19:00 +0800

    #Update Project10

---
+++ Project10/src/main.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <map>
+
+typedef void (*SkillFunc)();
+
+void fireball() { std::cout << "Fire ball >>>" << std::endl; }
+void heal() { std::cout << "Recover life <<<" << std::endl; }
+void shield() { std::cout << "Generate shield <<>>" << std::endl; }
+
+void castSkill(int key, std::map<int, SkillFunc> skillMap)
+{
+	if (skillMap.find(key) != skillMap.end())
+	{
+		skillMap[key](); // 调用对应的技能函数
+	}
+}
+
+
+int main(int argc, const char* argv[])
+{
+
+	std::map<int, SkillFunc> skillMap;
+	
+	skillMap[1] = &fireball;
+	skillMap[2] = &heal;
+	skillMap[3] = &shield;
+
+	castSkill(1, skillMap);
+	castSkill(2, skillMap);
+	castSkill(3, skillMap);
+
+	return 0;
+}
